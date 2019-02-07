98bb7a01c7380199b0762114cd12cc0627668ef8
Jorge Navarro <jnavarr56@gmail.com>
2019-02-07 17:10:38 -0500

    Added part one of HW2

---
+++ HW2/GetEvenFactorialSum.java
@@ -0,0 +1,91 @@
+import java.util.Scanner; 
+
+class GetEvenFactorialSum {
+
+  public static boolean isNumeric(String strNum) {
+
+    try {
+        double d = Double.parseDouble(strNum);
+
+    } 
+    catch (NumberFormatException | NullPointerException nfe) {
+
+        return false;
+    }
+
+    return true;
+
+  }
+
+  public static void main(String[] args) {
+
+    String input, bar = "==========================";
+    String dismissalText = "\nSee you later!\n\n" + bar;
+
+    System.out.println(bar);
+
+    System.out.println("Enter an integer that is greater or equal to 2");
+    System.out.println("and hit enter. I will calculate the sum of all ");
+    System.out.println("of the EVEN integers between 2 and that number.");
+    System.out.println("");
+    System.out.println("You can also type STOP to end this program.");
+     
+    input = new Scanner(System.in).next();
+
+    if (input.equals("STOP")) { 
+
+      System.out.println(dismissalText);
+
+      return; 
+    
+    }
+
+    while (!isNumeric(input) || Integer.parseInt(input) < 2) {
+
+      System.out.println("That's not a valid input. Please try again or type STOP to end this program.");
+
+      input = new Scanner(System.in).next();
+
+      if (input.equals("STOP")) { 
+
+        System.out.println(dismissalText);
+        
+        return; 
+
+      }
+
+    }
+
+    int total = 0, old;
+
+    System.out.println("\nStarting Total: " + total + "\n");
+
+    for(int i = 3; i < Integer.parseInt(input); i++) {
+
+      if (i % 2 == 0) { 
+
+        old = total;
+
+        total += i; 
+
+        System.out.println("\nAdding: " + i + " to Total: " + old + " for New Total: " + total + "\n");
+
+        
+      }
+
+      else {
+
+        System.out.println("(skipping odd number: " + i + ")");
+
+      }
+
+    }
+
+    System.out.println("\n TOTAL SUM: " + total);
+
+    System.out.println(bar);
+
+  }
+
+  
+}
---
+++ HW2/HW2.md
@@ -0,0 +1,26 @@
+# Assignment #2
+
+
+
+### 1) [40 points] Design and implement an application that reads an integer value and prints out the sum of all EVEN integers between 2 and its input value, inclusive. Print an error message if the input value is less than 2.  Prompt accordingly.
+
+### 2) [60 points] Computers are playing an increasing role in education.  Write a program that will help elementary school children learn multiplication.  Use the Random class in the java.util package to produce two positive one-digit integers.  The format to generating
+## this number is as follows:
+
+```
+    import java.util.* ;
+
+   /***  in your program ***/
+     
+     Random  myRandomNumber = new Random();
+     int myOneDigitNumber = Math.abs( myRandomNumber.nextInt() % 10 );
+```
+
+### The Math class in the java.lang package is also used to convert the number to an absolute value.  The modulus is used to get a single digit number.
+
+### Use this one digit number produced to type the question as follows:
+
+     How much is 6 times 7?
+
+### The student then types the answer.  Your program checks the student's answer.  If it is correct, print "Very Good!" and then ask another multiplication question.  If the answer
+### is wrong, print "No.  Please try again." and then let the student try the same question again ### repeatedly until the student finally gets it right.  Enter a -1 to exit program.
