ac65d3d5a5207d68e0d86b5d3d05b5b9c34958cd
Jorge Navarro <jnavarr56@gmail.com>
2019-02-28 00:13:22 -0500

    Added comments.

--- HW5/TriangleTest.java
+++ HW5/TriangleTest.java
@@ -3,14 +3,45 @@ import HW5.Triangle;
 public class TriangleTest {
     public static void main(String args[]) {
 
-        Triangle myTestTriangle = new Triangle(3, 4, 5);
+        System.out.println("\n\n");
 
-        System.out.println(myTestTriangle.isScalene());
-        System.out.println(myTestTriangle.isIsoceles());
-        System.out.println(myTestTriangle.isEquilateral());
-        System.out.println(myTestTriangle.calculatePerimeter());
+        //Test 1
 
-        
+        Triangle myTestTriangle1 = new Triangle(3, 4, 5);
+
+        System.out.println(myTestTriangle1.isScalene());
+        System.out.println(myTestTriangle1.isIsoceles());
+        System.out.println(myTestTriangle1.isEquilateral());
+        System.out.println(myTestTriangle1.calculatePerimeter());
+
+        System.out.println("\n\n");
+
+        //=======================================================
+
+        //Test 2
+
+        Triangle myTestTriangle2 = new Triangle(4, 4, 5);
+
+        System.out.println(myTestTriangle2.isScalene());
+        System.out.println(myTestTriangle2.isIsoceles());
+        System.out.println(myTestTriangle2.isEquilateral());
+        System.out.println(myTestTriangle2.calculatePerimeter());
+
+        System.out.println("\n\n");
+
+        //=======================================================
+
+        //Test 3
+
+        Triangle myTestTriangle3 = new Triangle(4, 4, 4);
+
+        System.out.println(myTestTriangle3.isScalene());
+        System.out.println(myTestTriangle3.isIsoceles());
+        System.out.println(myTestTriangle3.isEquilateral());
+        System.out.println(myTestTriangle3.calculatePerimeter());
+
+        System.out.println("\n\n");
+
     }
 
 }
